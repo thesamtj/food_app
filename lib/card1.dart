be1a3668a0d3a8d54fe3dc2c83d725318a6cd4ef
Samuel Tijani <omalitijani@gmail.com>
2022-01-25 00:05:55 +0100

    Add image

--- lib/card1.dart
+++ lib/card1.dart
@@ -14,7 +14,28 @@ class Card1 extends StatelessWidget {
     // 3
     return Center(
       // TODO: Card1 Decorate Container
-      child: Container(),
+      child: Container(
+        // TODO: Add a stack of text
+        // 1
+        padding: const EdgeInsets.all(16),
+        // 2
+        constraints: const BoxConstraints.expand(
+          width: 350,
+          height: 450,
+        ),
+        // 3
+        decoration: const BoxDecoration(
+          // 4
+          image: DecorationImage(
+            // 5
+            image: AssetImage('assets/mag1.png'),
+            // 6
+            fit: BoxFit.cover,
+          ),
+          // 7
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+        ),
+      ),
     );
   }
 }
