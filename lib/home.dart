dfe705b9a7cf45544a24b9bee987140283b82f53
Samuel Tijani <omalitijani@gmail.com>
2022-01-24 18:39:32 +0100

    Add bottom navigation bar

--- lib/home.dart
+++ lib/home.dart
@@ -27,6 +27,27 @@ class _HomeState extends State<Home> {
               // 3
               style: Theme.of(context).textTheme.headline1)),
       // TODO: Add bottom navigation bar
+      // 4
+      bottomNavigationBar: BottomNavigationBar(
+        // 5
+        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+        // TODO: Set selected tab bar
+        // 6
+        items: <BottomNavigationBarItem>[
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card2',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card3',
+          ),
+        ],
+      ),
     );
   }
 }
