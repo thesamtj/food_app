02fa096f890540c360e6de50702ed49eb16f6d7a
Samuel Tijani <omalitijani@gmail.com>
2022-01-25 16:33:59 +0100

    Create FriendPostListView

---
+++ lib/components/friend_post_list_view.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../models/models.dart';
+import 'components.dart';
+
+class FriendPostListView extends StatelessWidget {
+  // 1
+  final List<Post> friendPosts;
+
+  const FriendPostListView({
+    Key? key,
+    required this.friendPosts,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // 2
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 16,
+        right: 16,
+        top: 0,
+      ),
+      // 3
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // 4
+          Text('Social Chefs 👩‍🍳',
+              style: Theme.of(context).textTheme.headline1),
+          // 5
+          const SizedBox(height: 16),
+          // TODO: Add PostListView here
+          // 6
+          const SizedBox(height: 16),
+        ],
+      ),
+    );
+  }
+}
