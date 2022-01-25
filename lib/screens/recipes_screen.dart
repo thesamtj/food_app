852c842a3e8673f0e70e38dbf2ea2f3aeed2f5ef
Samuel Tijani <omalitijani@gmail.com>
2022-01-25 22:37:53 +0100

    Use the recipes GridView

--- lib/screens/recipes_screen.dart
+++ lib/screens/recipes_screen.dart
@@ -21,7 +21,7 @@ class RecipesScreen extends StatelessWidget {
         if (snapshot.connectionState == ConnectionState.done) {
           // TODO: Add RecipesGridView Here
           // 5
-          return const Center(child: Text('Recipes Screen'));
+          return RecipesGridView(recipes: snapshot.data ?? []);
         } else {
           // 6
           return const Center(child: CircularProgressIndicator());
