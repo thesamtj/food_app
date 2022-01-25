a21e2aa8f25ebbd6c96c211b605f2ce3e14890fb
Samuel Tijani <omalitijani@gmail.com>
2022-01-25 23:44:43 +0100

    Replace with EmptyGroceryScreen

--- lib/screens/grocery_screen.dart
+++ lib/screens/grocery_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'empty_grocery_screen.dart';
 
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({Key? key}) : super(key: key);
@@ -6,7 +7,8 @@ class GroceryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO 2: Replace with EmptyGroceryScreen
-    return Container(color: Colors.green);
+    // TODO 4: Add a scaffold widget
+    return const EmptyGroceryScreen();
   }
 // TODO: Add buildGroceryScreen
 }
