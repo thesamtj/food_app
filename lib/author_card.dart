8053afd94d8da63ce84eabc908f9e34ffd48545d
Samuel Tijani <omalitijani@gmail.com>
2022-01-25 11:07:17 +0100

    Manage state

--- lib/author_card.dart
+++ lib/author_card.dart
@@ -20,6 +20,8 @@ class AuthorCard extends StatefulWidget {
 }
 
 class _AuthorCardState extends State<AuthorCard> {
+  bool _isFavorited = false;
+
   @override
   Widget build(BuildContext context) {
     // TODO: Replace return Container(...);
@@ -56,15 +58,18 @@ class _AuthorCardState extends State<AuthorCard> {
           ),
           // TODO 2: add IconButton
           IconButton(
-              // 4
-              icon: const Icon(Icons.favorite_border),
-              iconSize: 30,
-              color: Colors.grey[400],
-              // 5
-              onPressed: () {
-                const snackBar = SnackBar(content: Text('Favorite Pressed'));
-                ScaffoldMessenger.of(context).showSnackBar(snackBar);
-              }),
+            // 1
+            icon: Icon(_isFavorited ? Icons.favorite : Icons.favorite_border),
+            iconSize: 30,
+            // 2
+            color: Colors.red[400],
+            onPressed: () {
+              // 3
+              setState(() {
+                _isFavorited = !_isFavorited;
+              });
+            },
+          ),
         ],
       ),
     );
