5f968e2c0942a8cf7d19c62a6870091d7e9224c9
Samuel Tijani <omalitijani@gmail.com>
2022-01-25 12:30:00 +0100

    Update

--- lib/components/author_card.dart
+++ lib/components/author_card.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'fooderlich_theme.dart';
+
+import '../fooderlich_theme.dart';
 import 'circle_image.dart';
 
 class AuthorCard extends StatefulWidget {
-  // 1
   final String authorName;
   final String title;
   final ImageProvider? imageProvider;
@@ -16,7 +16,7 @@ class AuthorCard extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  State<AuthorCard> createState() => _AuthorCardState();
+  _AuthorCardState createState() => _AuthorCardState();
 }
 
 class _AuthorCardState extends State<AuthorCard> {
@@ -24,23 +24,18 @@ class _AuthorCardState extends State<AuthorCard> {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Replace return Container(...);
     return Container(
       padding: const EdgeInsets.all(16),
       child: Row(
-        // TODO 3: add alignment
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          // 1
           Row(
             children: [
               CircleImage(
                 imageProvider: widget.imageProvider,
                 imageRadius: 28,
               ),
-              // 2
               const SizedBox(width: 8),
-              // 3
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -56,15 +51,11 @@ class _AuthorCardState extends State<AuthorCard> {
               ),
             ],
           ),
-          // TODO 2: add IconButton
           IconButton(
-            // 1
             icon: Icon(_isFavorited ? Icons.favorite : Icons.favorite_border),
             iconSize: 30,
-            // 2
             color: Colors.red[400],
             onPressed: () {
-              // 3
               setState(() {
                 _isFavorited = !_isFavorited;
               });
