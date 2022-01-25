9b57d7d2ea4cfdcad57f2fa77d94515fc1b74d26
Samuel Tijani <omalitijani@gmail.com>
2022-01-25 23:46:06 +0100

    Replace and add layout widgets

--- lib/screens/empty_grocery_screen.dart
+++ lib/screens/empty_grocery_screen.dart
@@ -6,6 +6,22 @@ class EmptyGroceryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO 3: Replace and add layout widgets
-    return Container(color: Colors.purple);
+    // 1
+    return Padding(
+      padding: const EdgeInsets.all(30.0),
+      // 2
+      child: Center(
+        // 3
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // TODO 4: Add empty image
+            // TODO 5: Add empty screen title
+            // TODO 6: Add empty screen subtitle
+            // TODO 7: Add browse recipes button
+          ],
+        ),
+      ),
+    );
   }
 }
