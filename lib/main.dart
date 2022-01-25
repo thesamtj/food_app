1b6833498801d546a5f7ded53645073892dc4ed6
Samuel Tijani <omalitijani@gmail.com>
2022-01-25 16:24:52 +0100

    Add the nested ListView

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class Fooderlich extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: Create theme
-    final theme = FooderlichTheme.light();
+    final theme = FooderlichTheme.dark();
     // TODO: Apply Home widget
     // 3
     return MaterialApp(
