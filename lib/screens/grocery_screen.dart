7e037a59d903d66707b47b857880581728e15e72
Samuel Tijani <omalitijani@gmail.com>
2022-01-26 00:41:56 +0100

    Add buildGroceryScreen

--- lib/screens/grocery_screen.dart
+++ lib/screens/grocery_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'empty_grocery_screen.dart';
+import 'package:provider/provider.dart';
+import '../models/models.dart';
 
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({Key? key}) : super(key: key);
@@ -10,5 +12,22 @@ class GroceryScreen extends StatelessWidget {
     // TODO 4: Add a scaffold widget
     return const EmptyGroceryScreen();
   }
+
 // TODO: Add buildGroceryScreen
+  Widget buildGroceryScreen() {
+    // 1
+    return Consumer<GroceryManager>(
+      // 2
+      builder: (context, manager, child) {
+        // 3
+        if (manager.groceryItems.isNotEmpty) {
+          // TODO 25: Add GroceryListScreen
+          return Container();
+        } else {
+          // 4
+          return const EmptyGroceryScreen();
+        }
+      },
+    );
+  }
 }
