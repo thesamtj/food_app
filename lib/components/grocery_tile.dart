3a70e2478c3dbe1f33dd485fcd6eec947b693c22
Samuel Tijani <omalitijani@gmail.com>
2022-01-26 16:09:42 +0100

    Add buildDate

--- lib/components/grocery_tile.dart
+++ lib/components/grocery_tile.dart
@@ -53,6 +53,14 @@ class GroceryTile extends StatelessWidget {
   }
 
 // TODO: Add buildDate()
+  Widget buildDate() {
+    final dateFormatter = DateFormat('MMMM dd h:mm a');
+    final dateString = dateFormatter.format(item.date);
+    return Text(
+      dateString,
+      style: TextStyle(decoration: textDecoration),
+    );
+  }
 
 // TODO: Add buildCheckbox()
 }
