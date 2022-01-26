43de0d5b7fffb9aaec65395617396ab337c8bccc
Samuel Tijani <omalitijani@gmail.com>
2022-01-26 13:11:32 +0100

    Add time picker

--- lib/screens/grocery_item_screen.dart
+++ lib/screens/grocery_item_screen.dart
@@ -104,6 +104,7 @@ class _GroceryItemScreenState extends State<GroceryItemScreen> {
             // TODO 15: Add date picker
             buildDateField(context),
             // TODO 16: Add time picker
+            buildTimeField(context),
             // TODO 17: Add color picker
             // TODO 18: Add slider
             // TODO: 19: Add Grocery Tile
@@ -255,6 +256,41 @@ class _GroceryItemScreenState extends State<GroceryItemScreen> {
   }
 
 // TODO: Add buildTimeField()
+  Widget buildTimeField(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Time of Day',
+              style: GoogleFonts.lato(fontSize: 28.0),
+            ),
+            TextButton(
+              child: const Text('Select'),
+              onPressed: () async {
+                // 1
+                final timeOfDay = await showTimePicker(
+                  // 2
+                  initialTime: TimeOfDay.now(),
+                  context: context,
+                );
+
+                // 3
+                setState(() {
+                  if (timeOfDay != null) {
+                    _timeOfDay = timeOfDay;
+                  }
+                });
+              },
+            ),
+          ],
+        ),
+        Text('${_timeOfDay.format(context)}'),
+      ],
+    );
+  }
 
 // TODO: Add buildColorPicker()
 
