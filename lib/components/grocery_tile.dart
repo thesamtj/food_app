e18d53ab741930b8a3ab454e0afd8988c4a0cfc2
Samuel Tijani <omalitijani@gmail.com>
2022-01-26 16:01:04 +0100

    Add Grocery Tile

---
+++ lib/components/grocery_tile.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+import '../models/grocery_item.dart';
+
+class GroceryTile extends StatelessWidget {
+  // 1
+  final GroceryItem item;
+  // 2
+  final Function(bool?)? onComplete;
+  // 3
+  final TextDecoration textDecoration;
+
+  // 4
+  GroceryTile({
+    Key? key,
+    required this.item,
+    this.onComplete,
+  })  : textDecoration =
+            item.isComplete ? TextDecoration.lineThrough : TextDecoration.none,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO 21: Change this Widget
+    return Container(
+      height: 100.0,
+      // TODO 20: Replace this color
+      color: Colors.red,
+    );
+  }
+
+// TODO: Add BuildImportance()
+  Widget buildImportance() {
+    if (item.importance == Importance.low) {
+      return Text('Low', style: GoogleFonts.lato(decoration: textDecoration));
+    } else if (item.importance == Importance.medium) {
+      return Text('Medium',
+          style: GoogleFonts.lato(
+              fontWeight: FontWeight.w800, decoration: textDecoration));
+    } else if (item.importance == Importance.high) {
+      return Text(
+        'High',
+        style: GoogleFonts.lato(
+          color: Colors.red,
+          fontWeight: FontWeight.w900,
+          decoration: textDecoration,
+        ),
+      );
+    } else {
+      throw Exception('This importance type does not exist');
+    }
+  }
+
+// TODO: Add buildDate()
+
+// TODO: Add buildCheckbox()
+}
--- lib/screens/grocery_item_screen.dart
+++ lib/screens/grocery_item_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
 import '../models/models.dart';
+import '../components/grocery_tile.dart';
 
 class GroceryItemScreen extends StatefulWidget {
   // 1
@@ -112,6 +113,22 @@ class _GroceryItemScreenState extends State<GroceryItemScreen> {
             const SizedBox(height: 10.0),
             buildQuantityField(),
             // TODO: 19: Add Grocery Tile
+            GroceryTile(
+              item: GroceryItem(
+                id: 'previewMode',
+                name: _name,
+                importance: _importance,
+                color: _currentColor,
+                quantity: _currentSliderValue,
+                date: DateTime(
+                  _dueDate.year,
+                  _dueDate.month,
+                  _dueDate.day,
+                  _timeOfDay.hour,
+                  _timeOfDay.minute,
+                ),
+              ),
+            ),
           ],
         ),
       ),
