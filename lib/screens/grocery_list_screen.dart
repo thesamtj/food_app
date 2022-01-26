4df93932fca3d7d893619ff0490c23d40f706783
Samuel Tijani <omalitijani@gmail.com>
2022-01-26 17:07:40 +0100

    Wrap in a Dismissable

--- lib/screens/grocery_list_screen.dart
+++ lib/screens/grocery_list_screen.dart
@@ -29,35 +29,55 @@ class GroceryListScreen extends StatelessWidget {
           // TODO 27: Wrap in an InkWell
           // 5
           // 1
-          return InkWell(
-            child: GroceryTile(
-                key: Key(item.id),
-                item: item,
-                onComplete: (change) {
-                  if (change != null) {
-                    manager.completeItem(index, change);
-                  }
-                }),
-            // 2
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => GroceryItemScreen(
-                    originalItem: item,
-                    // 3
-                    onUpdate: (item) {
-                      // 4
-                      manager.updateItem(item, index);
-                      // 5
-                      Navigator.pop(context);
-                    },
-                    // 6
-                    onCreate: (item) {},
-                  ),
-                ),
-              );
+          return Dismissible(
+            // 6
+            key: Key(item.id),
+            // 7
+            direction: DismissDirection.endToStart,
+            // 8
+            background: Container(
+                color: Colors.red,
+                alignment: Alignment.centerRight,
+                child: const Icon(Icons.delete_forever,
+                    color: Colors.white, size: 50.0)),
+            // 9
+            onDismissed: (direction) {
+              // 10
+              manager.deleteItem(index);
+              // 11
+              ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('${item.name} dismissed')));
             },
+            child: InkWell(
+              child: GroceryTile(
+                  key: Key(item.id),
+                  item: item,
+                  onComplete: (change) {
+                    if (change != null) {
+                      manager.completeItem(index, change);
+                    }
+                  }),
+              // 2
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => GroceryItemScreen(
+                      originalItem: item,
+                      // 3
+                      onUpdate: (item) {
+                        // 4
+                        manager.updateItem(item, index);
+                        // 5
+                        Navigator.pop(context);
+                      },
+                      // 6
+                      onCreate: (item) {},
+                    ),
+                  ),
+                );
+              },
+            ),
           );
         },
         // 8
