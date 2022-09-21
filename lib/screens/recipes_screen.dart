38e656424bb6403054e95310aa35b5272da45e3a
Samuel Tijani <54986149+thesamtj@users.noreply.github.com>
2022-09-21 21:20:29 +0100

    Update recipes_screen.dart

--- lib/screens/recipes_screen.dart
+++ lib/screens/recipes_screen.dart
@@ -5,16 +5,16 @@ import '../api/mock_fooderlich_service.dart';
 import '../components/components.dart';
 
 class RecipesScreen extends StatelessWidget {
-  // 1
+  
   final exploreService = MockFooderlichService();
 
   RecipesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // 2
+    
     return FutureBuilder(
-      // 3
+     
       future: exploreService.getRecipes(),
       builder: (context, AsyncSnapshot<List<SimpleRecipe>> snapshot) {
         // 4
