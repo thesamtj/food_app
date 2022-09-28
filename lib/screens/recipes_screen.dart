f922a3a62b633fa252c5a57292cd080f55949692
Samuel Tijani <54986149+thesamtj@users.noreply.github.com>
2022-09-28 22:33:34 +0100

    Update recipes_screen.dart

--- lib/screens/recipes_screen.dart
+++ lib/screens/recipes_screen.dart
@@ -17,13 +17,12 @@ class RecipesScreen extends StatelessWidget {
      
       future: exploreService.getRecipes(),
       builder: (context, AsyncSnapshot<List<SimpleRecipe>> snapshot) {
-        // 4
+       
         if (snapshot.connectionState == ConnectionState.done) {
-          // TODO: Add RecipesGridView Here
-          // 5
+         
           return RecipesGridView(recipes: snapshot.data ?? []);
         } else {
-          // 6
+          
           return const Center(child: CircularProgressIndicator());
         }
       },
