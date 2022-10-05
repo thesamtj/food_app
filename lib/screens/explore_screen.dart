494905090b11dc9f6909b6029c881c0f752107a1
Samuel Tijani <54986149+thesamtj@users.noreply.github.com>
2022-10-05 17:17:24 +0100

    Update explore_screen.dart

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -43,11 +43,11 @@ class _ExploreScreenState extends State<ExploreScreen> {
 
   @override
   Widget build(BuildContext context) {
-    // 1
+    
     return FutureBuilder(
-      // 2
+    
       future: mockService.getExploreData(),
-      // 3
+     
       builder: (context, AsyncSnapshot<ExploreData> snapshot) {
         // 4
         if (snapshot.connectionState == ConnectionState.done) {
